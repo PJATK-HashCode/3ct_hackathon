38dd175c51c68bfd8f397eecf2e6f5369ff8ebcc
jMerta <mer.kuba@gmail.com>
2017-05-27 16:21:29 +0200

    Initial commit

--- src/main/java/model/user.java
+++ src/main/java/model/user.java
@@ -1,7 +1,20 @@
 package model;
 
+import lombok.Data;
+
 /**
  * Created by Kuba on 27.05.2017.
  */
+@Data
 public class user {
+
+    private int id;
+    private String password;
+    private String name;
+    private String surname;
+    private String phoneNumber;
+    private String email;
+    private String nickname;
+
+
 }
