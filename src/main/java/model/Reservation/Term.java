8624904058183b3db49af15a0509254b6c121d6a
jMerta <mer.kuba@gmail.com>
2017-05-27 16:41:22 +0200

    Added Joda Time

--- src/main/java/model/Reservation/Term.java
+++ src/main/java/model/Reservation/Term.java
@@ -1,7 +1,13 @@
 package model.Reservation;
 
+import lombok.Data;
+import org.joda.time.LocalTime;
+
 /**
  * Created by Kuba on 27.05.2017.
  */
+@Data
 public class Term {
+    private Boolean IsTaken;
+    private LocalTime dateTime;
 }
