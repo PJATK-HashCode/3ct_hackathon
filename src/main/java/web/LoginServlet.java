67a11b99a65db70dbe55280c09ebb6f4e4ee24da
Lelental <p.lelental@gmail.com>
2017-05-27 23:48:45 +0200

    security #2

--- src/main/java/web/LoginServlet.java
+++ src/main/java/web/LoginServlet.java
@@ -1,6 +1,8 @@
 package web;
 
 import dao.service.RepositoryService;
+import model.user.User;
+import security.Encryptor;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * @author Lelental on 27.05.2017.
@@ -21,14 +24,20 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String nick = req.getParameter("nick");
+        String password= req.getParameter("password");
         RepositoryService repositoryService = new RepositoryService();
         //TODO Uwierzytelnianie
-        if (repositoryService.users().withNick(nick) != null) {
-            HttpSession session = req.getSession();
-            session.setAttribute(SessionKey.login, repositoryService.users().withNick(nick));
-            resp.sendRedirect("/place.jsp");
-        } else {
-            resp.getWriter().println("Zjebałes, nie elo");
+        User user = repositoryService.users().withNick(nick);
+        try {
+            if (user != null && user.getPassword() == Encryptor.get_SHA_256_SecurePassword(password,Encryptor.getSalt())) {
+                HttpSession session = req.getSession();
+                session.setAttribute(SessionKey.login, repositoryService.users().withNick(nick));
+                resp.sendRedirect("/place.jsp");
+            } else {
+                resp.getWriter().println("Zjebałes, nie elo");
+            }
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
         }
     }
 }
